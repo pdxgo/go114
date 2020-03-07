19881fb7b982cd1118eadd2c433688cac560c91b
logan <444193+brainsnail@users.noreply.github.com>
2020-03-07 08:02:35 -0800

    Update preemption example

--- preemption/main.go
+++ preemption/main.go
@@ -1,15 +1,32 @@
+// Demonstrates async preemption in 1.14 vs. earlier versions
 package main
 
 import (
-	"fmt"
+	"log"
+	"runtime"
+	"time"
 )
 
-func gr1() {
+func f1() {
+	var f float64 = 1.0
+	for i := 0; i < 1000000000; i++ {
+		f = (f + float64(i)) / f
+	}
+	log.Println("f1", f)
+}
+
+func f2() {
 	for {
+		time.Sleep(100 * time.Millisecond)
+		log.Println("f2")
 	}
 }
 
 func main() {
-	go gr1()
-	fmt.Println("Hello from main")
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	runtime.GOMAXPROCS(1)
+	go f2()
+	time.Sleep(300 * time.Millisecond)
+	go f1()
+	select {}
 }
